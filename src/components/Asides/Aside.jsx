57478cf856e3899d778f5474c467b476be390915
Shailseen <shailesh13246@gmail.com>
2022-04-20 01:14:29 +0530

    feat: add HomePage

---
+++ src/components/Asides/Aside.jsx
@@ -0,0 +1,12 @@
+import { useAside } from "../../context/aside-context";
+import { IconAside } from "./IconAside/IconAside";
+import { OpenAside } from "./OpenAside/OpenAside";
+
+export const Aside = () => {
+  return (
+    <>
+    <IconAside/>
+    <OpenAside/>
+    </>
+  )
+};
