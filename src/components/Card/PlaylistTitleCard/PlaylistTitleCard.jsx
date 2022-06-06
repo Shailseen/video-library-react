c123ac96a8b974fe28737b52af4eee139248652f
Shailseen <shailesh13246@gmail.com>
2022-06-06 22:36:02 +0530

    feat: playlist delete service

--- src/components/Card/PlaylistTitleCard/PlaylistTitleCard.jsx
+++ src/components/Card/PlaylistTitleCard/PlaylistTitleCard.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { useVideo } from "../../../context/videos-context";
 import styles from "./PlaylistTitleCard.module.css";
 import DeleteIcon from "@mui/icons-material/Delete";
+import deletePlaylistService from "../../../services/PlaylistServices/deletePlaylistService";
 const PlaylistTitleCard = ({ video }) => {
+  const { setPlaylistCategories } = useVideo();
   const { _id, title, videos } = video;
+  const deletePlaylistHandler = () => {
+    deletePlaylistService(_id, setPlaylistCategories);
+  };
   return (
     <div key={_id} className={styles.container}>
       <div className={styles.title}>
         <h2>{title}</h2>
         <p className={styles.video_count}>{videos.length} videos</p>
       </div>
-      <div className={styles.delete_playlist}>
+      <div className={styles.delete_playlist} onClick={deletePlaylistHandler}>
         <DeleteIcon sx={{ fontSize: "30px" }} />
       </div>
     </div>
